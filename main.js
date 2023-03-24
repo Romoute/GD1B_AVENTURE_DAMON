21e29bdca8cfbf1321312709b5fea0ee91830f85
Romoute <r.damon@rns.ecolescreatives.com>
2023-03-24 23:19:13 +0100

    ERZET

--- main.js
+++ main.js
@@ -15,5 +15,32 @@ var config = {
     scene: [scene_potager, scene_champ, scene_poule, scene_finale] 
 };
 
-
 var game = new Phaser.Game(config);
+
+
+function controlPlayer(player, cursors){
+
+    player.body.velocity.normalize()
+  //DEPLACEMENTS DU JOUEUR
+    if (cursors.up.isDown) {
+        player.setVelocityY(-player.speed); 
+        player.anims.play(player.currentAnims[1], true); 
+        player.dir = "up";
+        
+    }
+    else if (cursors.down.isDown) {       
+        player.setVelocityY(player.speed); 
+        player.anims.play(player.currentAnims[3], true); 
+        player.dir = "down";     
+    }
+    else if (cursors.left.isDown) { 
+        player.setVelocityX(-player.speed); 
+        player.anims.play(player.currentAnims[0], true); 
+        player.dir = "left";
+    }
+    else if (cursors.right.isDown) { 
+        player.setVelocityX(player.speed); 
+        player.anims.play(player.currentAnims[2], true); 
+        player.dir = "right";
+    }
+}
