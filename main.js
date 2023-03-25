11ff9d62d32ab320fc91d2ddec2f1a99b5004d26
Romoute <r.damon@rns.ecolescreatives.com>
2023-03-25 19:46:54 +0100

    qetr

--- main.js
+++ main.js
@@ -24,23 +24,19 @@ function controlPlayer(player, cursors){
   //DEPLACEMENTS DU JOUEUR
     if (cursors.up.isDown) {
         player.setVelocityY(-player.speed); 
-        player.anims.play(player.currentAnims[1], true); 
         player.dir = "up";
         
     }
     else if (cursors.down.isDown) {       
         player.setVelocityY(player.speed); 
-        player.anims.play(player.currentAnims[3], true); 
         player.dir = "down";     
     }
     else if (cursors.left.isDown) { 
         player.setVelocityX(-player.speed); 
-        player.anims.play(player.currentAnims[0], true); 
         player.dir = "left";
     }
     else if (cursors.right.isDown) { 
         player.setVelocityX(player.speed); 
-        player.anims.play(player.currentAnims[2], true); 
         player.dir = "right";
     }
 }
