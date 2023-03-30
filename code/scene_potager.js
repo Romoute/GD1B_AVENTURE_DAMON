858bf66123649f461bb3b0bfd0326cfe716e33dd
Romoute <r.damon@rns.ecolescreatives.com>
2023-03-30 17:18:01 +0200

    ajouts assets

--- code/scene_potager.js
+++ code/scene_potager.js
@@ -28,8 +28,16 @@ class scene_potager extends Phaser.Scene {
         this.load.spritesheet('carotte_gauche', 'assets/carotte_gauche.png',
             { frameWidth: 32, frameHeight: 64});
         this.load.spritesheet('carotte_droite', 'assets/carotte_droite.png',
-            { frameWidth: 32, frameHeight: 64});        
-        this.load.spritesheet('pdt_face', 'assets/pdt_face.png',
+            { frameWidth: 32, frameHeight: 64});     
+            this.load.spritesheet('carotte_face_econome', 'assets/carotte_face_econome.png',
+            { frameWidth: 48, frameHeight: 80});
+        this.load.spritesheet('carotte_dos_econome', 'assets/carotte_dos_econome.png',
+            { frameWidth: 48, frameHeight: 80});    
+        this.load.spritesheet('carotte_gauche_econome', 'assets/carotte_gauche_econome.png',
+            { frameWidth: 64, frameHeight: 64});
+        this.load.spritesheet('carotte_droite_econome', 'assets/carotte_droite_econome.png',
+            { frameWidth: 64, frameHeight: 64});   
+        this.load.spritesheet('pdt_face_econome', 'assets/pdt_face_econome.png',
             { frameWidth: 32, frameHeight: 48});
         
         this.load.image('econome', 'assets/econome.png');
@@ -42,8 +50,16 @@ class scene_potager extends Phaser.Scene {
 
     create(){
         
+        //CREATION MAP ET TILESET
         this._tilemap = this.add.tilemap("map");
         this._tileset = this._tilemap.addTilesetImage("tileset_1");
+
+
+
+
+        //COLLISIONS TILESET
+
+
 
     }
 
